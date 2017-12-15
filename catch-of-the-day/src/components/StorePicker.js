f0949c7858381ae8443542b995d33d7a9dc93e87
Becky Bolton <bexb1@hotmail.com>
2017-12-15 17:21:01 +0000

    First component built

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { format } from 'path';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form class="store-selector">
+        
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
