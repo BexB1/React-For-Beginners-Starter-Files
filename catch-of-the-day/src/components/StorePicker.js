1af7006f45f9cde2d027b6f88d528f878e845419
Becky Bolton <bexb1@hotmail.com>
2018-01-08 11:47:07 +0000

    Several components built.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,7 @@ import { format } from 'path';
 class StorePicker extends React.Component {
   render() {
     return (
-      <form class="store-selector">
+      <form className="store-selector">
         { /* I'm a JSX comment. I must go in the parent element, in this syntax */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" />
