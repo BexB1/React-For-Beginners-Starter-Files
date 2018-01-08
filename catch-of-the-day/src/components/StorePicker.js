51f49dce99730d1df331f32f8f7064ee96ea494f
Becky Bolton <bexb1@hotmail.com>
2018-01-08 17:37:44 +0000

    fishes state added.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { format } from 'path';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log("new URL!");
+
+    const storeId = this.storeInput.value;
+
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         { /* I'm a JSX comment. I must go in the parent element, in this syntax */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/>
         <button type="submit">Visit Store >></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
