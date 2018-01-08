b0b31f6fd0d3b0c057444a691c8e634ec3b1b8c4
Becky Bolton <bexb1@hotmail.com>
2018-01-08 11:06:36 +0000

    Store search JSX written, CSS linked.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,7 +5,10 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form class="store-selector">
-        
+        { /* I'm a JSX comment. I must go in the parent element, in this syntax */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store >></button>
       </form>
     )
   }
